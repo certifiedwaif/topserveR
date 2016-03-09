efdad8d23403f35e8aaf14eabdab1b3148afc545
Mark Greenaway <markg@maths.usyd.edu.au>
2016-03-09 21:19:40 +1100

    Updates.

---
+++ R/topserveR.R
@@ -0,0 +1,12 @@
+#'@title Package-level documentation
+#'@description Query Paul Szabo's topserver page
+#'
+#'@name topserveR
+#'
+#'@import stringr
+#'@import RCurl
+#'@import purrr
+#'@docType package
+#'@aliases topserveR topserveR-package
+
+NULL
